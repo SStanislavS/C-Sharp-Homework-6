ef3c9945e1976ab3cafaf940f7f84067a0f541f8
Stanislav <S@mail.ru>
2022-12-19 13:14:51 +0300

    add Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 ввел пользователь.
+Console.Write("Введите c клавиатуры числа (через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Количествово элементов больше 0 ввел пользователь: " + count);
