fef3c5f1c822cbc922a81bfd39a66efd4b6942b8
Stanislav <S@mail.ru>
2022-12-20 20:23:01 +0300

    add Task 2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -9,7 +9,7 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение k2: ");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
-double x = (-b2 + b1)/(-k1 + k2);
+double x = (b1-b2)/(k2-k1);
 double y = k2 * x + b2;
 
 Console.WriteLine($"Две прямые пересекутся в точке: {x}; {y}");
