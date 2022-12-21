331a5cb9b819719b1d17a9263ea3def520983a3d
Stanislav <S@mail.ru>
2022-12-21 20:01:23 +0300

    add Task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найдите сумму произведений пар чисел в одномерном целочисленном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+Console.Write("Введите с клавиатуры числа (через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int sum = 0;
+
+for (int i = 0; i <arr.Length / 2; i++)
+{
+     sum += arr[i] * arr[arr.Length - 1 - i];
+}
+ 
+Console.WriteLine("Сумма произведений пар чисел: " + sum);
+ 
